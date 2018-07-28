42a0c44782779ba2044722f87cc2b1a2509bd65f
osagaw <jianghao7625@gmail.com>
2018-07-28 15:56:27 +0800

    commit code comment

--- src/main/java/io/Echo.java
+++ src/main/java/io/Echo.java
@@ -8,10 +8,12 @@ import java.io.*;
 
 public class Echo {
 	public static void main(String[] args) throws IOException {
+		// 按字符输入
 		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 		String s;
 		while ((s = stdin.readLine()) != null && s.length() != 0)
 			System.out.println(s);
 		// An empty line or Ctrl-Z terminates the program
 	}
-} /// :~
+} 
+/// :~
