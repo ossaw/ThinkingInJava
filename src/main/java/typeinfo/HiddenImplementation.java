0b6573882316cd0c97464ff2cab1f165d9b7f27a
ossaw <jianghao7625@gmail.com>
2018-08-07 17:31:43 +0800

    commit code

--- src/main/java/typeinfo/HiddenImplementation.java
+++ src/main/java/typeinfo/HiddenImplementation.java
@@ -12,9 +12,10 @@ public class HiddenImplementation {
 		a.f();
 		System.out.println(a.getClass().getName());
 		// Compile error: cannot find symbol 'C':
-		/*
-		 * if(a instanceof C) { C c = (C)a; c.g(); }
-		 */
+		/*if (a instanceof C) {
+			C c = (C) a;
+			c.g();
+		}*/
 		// Oops! Reflection still allows us to call g():
 		callHiddenMethod(a, "g");
 		// And even methods that are less accessible!
